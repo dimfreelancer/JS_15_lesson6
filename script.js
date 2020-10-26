528ddb99230b16c97393baa90a13c1ca0cce1a19
Dmitry Freelancer <dimfreelancerdn@gmail.com>
2020-10-26 13:00:54 +0300

    Урок 7 Отдельный репозиторий подготовка

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+console.log('Hello this is the Gaming guess the number bot');
+
+
+var i = 10;
+/* 
+function one(a) {
+
+        console.log('i', i--, ' a ->', a);
+        if (i <= 0 || a > 1000) {
+            return 0;
+
+        } else {
+
+            one(a *= 2);
+        }
+    }
+one(555);
+ */
+
+const randomInt = () => {
+    
+    const result = Math.floor( (100 * Math.random()) + 1 );
+    console.log('arr: ', arr);
+
+    return result;
+}
+
